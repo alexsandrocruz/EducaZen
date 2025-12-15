71fa754d020072e2e7a4b3035cff15c16e1aecb4
AlexSandro Cruz <alexcruzti@gmail.com>
2025-12-15 19:30:33 -0300

    fix(angular): simplify ClassesModule to fix routing issue with standalone components

--- EstudaZen/angular/src/app/admin/classes/classes.module.ts
+++ EstudaZen/angular/src/app/admin/classes/classes.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ClassesRoutingModule } from './classes-routing.module';
-import { ClassListComponent } from './class-list.component';
-import { ClassFormComponent } from './class-form.component';
 
 @NgModule({
-    declarations: [],
     imports: [
-        CommonModule,
-        ClassesRoutingModule,
-        ClassListComponent,
-        ClassFormComponent,
-    ],
+        ClassesRoutingModule
+    ]
 })
 export class ClassesModule { }
